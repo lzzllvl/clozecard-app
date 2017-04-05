b6cc0a16a8559b6dd1f9155a59b32cac3b0d9fb6
Greg Lee <gml2136@gmail.com>
2017-04-05 15:51:28 -0700

    Added an add question function to the CLI, flag -a.

--- main.js
+++ main.js
@@ -4,54 +4,99 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 
-//I use readFileSync because the file is relatively short and I want to avoid the xmas tree
-const getCardArray = () => {
-  let result = fs.readFileSync('cards.json', "utf8");
-  let cards = JSON.parse(result);
-  let cardArray = [];
-  for(card in cards) {
-    if(cards[card].type == "flash") {
-      cardArray.push(FlashCard(cards[card].question, cards[card].answer));
-    } else if(cards[card].type == "cloze") {
-      cardArray.push(ClozeCard(cards[card].question, cards[card].answer));
-    }
-  }
-  return cardArray;
-}
 
-const doCLQuiz = (cardArr) => {
-  let num = cardArr.length;
-  let i = 0;
-  let ask = function() {
-    if(num > 0) {
-      let question = cardArr[i];
-      let mess = "";
-      if(question instanceof FlashCard){
-        mess = cardArr[i].getFront();
-      } else {
-        mess = cardArr[i].openOnly();
+const cardCLI = {
+  getCardArray: () => {
+    //I use readFileSync because the file is relatively short and
+    //I want to avoid deeply nesting callbacks
+    let result = fs.readFileSync('cards.json', "utf8");
+    let cards = JSON.parse(result);
+    let cardArray = [];
+    for(card in cards) {
+      if(cards[card].type == "flash") {
+        cardArray.push(FlashCard(cards[card].question, cards[card].answer));
+      } else if(cards[card].type == "cloze") {
+        cardArray.push(ClozeCard(cards[card].question, cards[card].answer));
       }
-      inquirer.prompt([
-        {
-          name: "response",
-          message: mess
-        }
-      ]).then(function(answers){
-        if(cardArr[i].check(answers.response)){
-          console.log("Correct!, Good Job");
-        } else {
-          console.log("Incorrect, read a book.");
-        }
-        i++;
-        num --;
-        ask();
-      });
-    } else {
-      console.log("Quiz Over!");
     }
+    return cardArray;
+  },
+
+  doCLQuiz: (cardArr) => {
+    let num = cardArr.length;
+    let i = 0;
+    let ask = function() {
+      if(num > 0) {
+        let question = cardArr[i];
+        let mess = "";
+        if(question instanceof FlashCard){
+          mess = cardArr[i].getFront();
+        } else {
+          mess = cardArr[i].openOnly();
+        }
+        inquirer.prompt([
+          {
+            name: "response",
+            message: mess
+          }
+        ]).then(function(answers){
+          if(cardArr[i].check(answers.response)){
+            console.log("Correct!, Good Job");
+          } else {
+            console.log("Incorrect, read a book.");
+          }
+          i++;
+          num --;
+          ask();
+        });
+      } else {
+        console.log("Quiz Over!");
+      }
+    }
+    return ask;
+  },
+  addQuestion: () => {
+    inquirer.prompt([
+      {
+        name: "type",
+        message: "Please Enter a Card type (Flash or Cloze)."
+      },
+      {
+        name: "question",
+        message: "Please Enter the Question/Full Text."
+      },
+      {
+        name: "answer",
+        message: "Please Enter the Answer/Cloze."
+      }
+    ]).then(answers => {
+      //ensure proper answers before writing.
+      let type = answers.type.toLowerCase();
+      answers.type = answers.type.toLowerCase();
+      if(type != "cloze" && type != "flash"){
+        console.log(`Error: type '${type}' is Not valid, Enter a valid card type.`);
+        return;
+      }
+      if(!answers.question.length){
+        console.log(`Error: Question Body is Not valid. Enter a valid Question.`);
+        return;
+      }
+      if(!answers.answer.length){
+        console.log(`Error: Answer is Not valid. Enter a valid Answer.`);
+        return;
+      }
+
+      let data = JSON.parse(fs.readFileSync("cards.json", "utf8"));
+      let nextCard = ++data.numCards;
+      data[nextCard] = answers;
+      fs.writeFile("cards.json", JSON.stringify(data, null, 2), err => {
+        err ? console.log(err) : console.log("Question Added");
+      });
+    })
   }
-  return ask;
 }
 
-doCLQuiz(getCardArray())();
+process.argv[2] == "-a"
+  ? cardCLI.addQuestion()
+  : cardCLI.doCLQuiz(cardCLI.getCardArray())();
 
